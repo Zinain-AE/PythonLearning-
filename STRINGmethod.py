95576a790d12d12083228549da480c5e199f88cc
Zinain Afrin Elma <zinainafrinelma@gmail.com>
2025-08-02 23:15:54 +0600

    Add files via upload

---
+++ STRINGmethod.py
@@ -0,0 +1,10 @@
+#Taking input from the user 
+name = input ("What's your name?\n")
+#Remove white space from the str
+name.strip()
+#Capitilize user name
+name.title()
+
+print (f"hello,{name}")
+
+#Also we can do this code as like this name= input ("What's your name?\n").strip().title()
