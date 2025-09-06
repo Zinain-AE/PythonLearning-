b0fdfb5058a17ca3b5e66c817db1752ef771070f
Zinain Afrin Elma <zinainafrinelma@gmail.com>
2025-09-06 22:59:31 +0600

    Add files via upload

---
+++ 2-Conditionals/2-if,else,elif.py
@@ -0,0 +1,15 @@
+x=int (input("Whats x ?"))
+y=int (input("Whats y ?"))
+
+if x<y:
+    print("x is less than y")
+
+elif x>y:
+    print("x is greater than y")
+elif x<=y:
+    print("x is less than or equal to y")
+
+elif x>=y:
+    print("x is greater than or equal to y")
+else:
+     print("x is equal to y")
