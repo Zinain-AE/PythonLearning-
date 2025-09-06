e0363c63ea98966ff8b5dc5ca0302112c7abd93b
Zinain Afrin Elma <zinainafrinelma@gmail.com>
2025-09-06 22:34:42 +0600

    Add files via upload

---
+++ 1-Hello world/4-def.py
@@ -0,0 +1,5 @@
+def hello(to="Deshbashi"):
+    print("Hello ," ,to)
+hello()
+name = input("What is your name? ")
+hello(name)
---
+++ 1-Hello world/5-def2.py
@@ -0,0 +1,7 @@
+def main():
+    x=int(input("What's X?\n"))
+    print("X is squared to ", square(x))
+
+def square(n):
+    return n*n #return value of square(x) , n**2 , pow(n,2,3..)
+main() #calling main function
