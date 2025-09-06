2a28c9a93925beee64c28230e695ca99a1fe4526
Zinain Afrin Elma <zinainafrinelma@gmail.com>
2025-09-06 23:01:52 +0600

    Delete match.py

--- match.py
+++
@@ -1,9 +0,0 @@
-name= input('Enter your name?')
-
-match name:
-    case "Wednesday" | "Modrica" | "Gomez" :
-        print("Addams")
-    case "Siren":
-        print("Nevermore")
-    case _:
-        print("Nope")
